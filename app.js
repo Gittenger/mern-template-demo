82b23a1b4d062d5cfba8eebdcc6f65528bca6e48
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-05-08 14:13:20 -0400

    attempting to deploy

--- app.js
+++ app.js
@@ -61,10 +61,10 @@ app.use('/', limiter)
 app.use(express.static(path.join(__dirname, 'public')))
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'))
+	app.use(express.static(path.join(__dirname, 'client', 'build')))
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 	})
 }
 
